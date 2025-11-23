a763f76b616ebdf1ccbbdd7fd9359049ac758816
GavinStudent <gavinpaul.dizon@ontariotechu.net>
2025-11-23 18:12:36 -0500

    user and register added

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,5 +12,61 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+// Get method for register
+router.get('/register', function(req,res,next){
+  if(!req.user)
+  {
+    res.render('auth/register',
+      {
+      title:'Register',
+      message: req.flash('registerMessage')
+      }
 
+    )
+  }
+  else
+  {
+    return res.redirect("/")
+  }
+});
+// Post method for register
+router.post('/register', function(req,res,next){
+  let newUser = new User({
+    username: req.body.username,
+    //password: req.body.password,
+    email:req.body.email,
+    displayName: req.body.displayName
+  })
+  User.register(newUser, req.body.password, (err)=>{
+    if(err)
+    {
+      console.log("Error:Inserting the new user");
+      if(err.name=="UserExistingError")
+      {
+        req.flash('registerMessage','Registration Error:User already Exist');
+      }
+      return res.render('auth/register',
+        {
+          title:'Register',
+          message:req.flash('registerMessage')
+        }
+      )
+    }
+    else{
+      return passport.authenticate('local')(req,res,()=>{
+        res.redirect("/movies");
+      })
+    }
+  })
+});
+router.get('/logout',function(req,res,next){
+req.logout(function(err)
+{
+  if(err)
+  {
+    return next(err)
+  }
+})
+res.redirect("/");
+})
 module.exports = router;
