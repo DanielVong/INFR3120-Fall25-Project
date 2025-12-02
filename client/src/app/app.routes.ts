9d421a4800509a21d46bfc3413f0d5c243fbf037
DanielVong <danielvong9@gmail.com>
2025-12-02 08:47:42 -0500

    Created base for Client-Side

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import {Home} from "./pages/home/home";
+import {Movies} from "./pages/movies/movies";
+export const routes: Routes = [
+    {path:'home', component:Home},
+    {path:'movies', component:Movies},
+    {path:'',component:Home}
+];
